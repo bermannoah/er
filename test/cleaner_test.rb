c6f7e75e36631c0e376782f75cca3a689e16249c
Noah Berman <noah@noahberman.org>
2016-09-06 11:42:42 -0600

    tests for cleaner more or less complete

--- test/cleaner_test.rb
+++ test/cleaner_test.rb
@@ -31,8 +31,31 @@ class CleanerTest < Minitest::Test
     assert_equal "00123", c.clean_zipcode(123)
   end
   
-  def test_cleaner_can_clean_a_phone_number
+  def test_cleaner_can_handle_a_phone_number_that_is_too_long
     c = Cleaner.new
-    assert_equal "Bad number entry.", c.clean_number("")
+    assert_equal "Bad number entry.", c.clean_number("4237840384701740834")
+  end
+  
+  def test_cleaner_can_handle_a_phone_number_that_is_too_long_and_starts_with_a_one
+    c = Cleaner.new
+    assert_equal 5558675309, c.clean_number("15558675309")
+  end
+  
+  def test_cleaner_can_handle_a_phone_number_that_is_too_long_and_does_not_start_with_a_one
+    c = Cleaner.new
+    assert_equal "Bad number entry.", c.clean_number("48151623421")
+  end
+  
+  def test_cleaner_can_handle_a_number_that_is_too_short
+    c = Cleaner.new
+    assert_equal "Bad number entry.", c.clean_number("4815")
+  end
+  
+  def test_cleaner_can_handle_a_number_that_is_just_right_but_has_special_chars
+    c = Cleaner.new
+    assert_equal 5555555555, c.clean_number("(555) 555 5555")
+  end
+  
+    
 
 end
