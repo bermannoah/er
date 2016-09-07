d8e5f874bc7019261a8437104da46dfb133c82f9
Noah Berman <noah@noahberman.org>
2016-09-06 18:45:45 -0600

    made a runner file that should work

---
+++ event_reporter.rb
@@ -0,0 +1,16 @@
+require './lib/repl'
+
+puts "Hello and welcome to Event Reporter, for all your event reporting needs."
+puts "If you're not sure what to do, enter 'help'."
+
+user_selection = ""
+loop do 
+  print "Enter command > "
+  user_selection = gets.chomp
+  choice = repl(user_selection)
+  break if choice == "quit" 
+  puts choice unless choice == "quit"
+end
+
+puts "Thanks for using Event Reporter! Bye now."
+  
--- lib/repl.rb
+++ lib/repl.rb
@@ -3,10 +3,6 @@ require 'pry'
 
 class Repl < Queue
   
-  puts "Hello there! What file would you like to load?"
-  puts "Enter 'load' to load the default or enter a file path to specify a different file."
-  puts "You can also enter 'help' or 'help' followed by a command name if you're not sure what to do."
-  
   
   
   
