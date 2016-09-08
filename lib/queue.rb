fde36a23573317054da2b1e314c619b555a6b077
Noah Berman <noah@noahberman.org>
2016-09-08 09:52:44 -0600

    sort of working html exporter, minor refactors

--- lib/queue.rb
+++ lib/queue.rb
@@ -10,19 +10,16 @@ require 'pry'
 
 class QueueHolder < Loader
   
-  attr_accessor :queue, :api_key, :client, :queue_district, :found_district, :district, :queue_count, :queue_results, :rows, :table, :table_printer, :html_template
+  attr_accessor :queue, :api_key, :client, :queue_district, :found_district, :district, :queue_count, :queue_results, :filename, :rows, :table, :table_printer, :exporter, :html_template
   
   HEADER_ROW = ["LAST NAME", "FIRST NAME", "EMAIL", "ZIPCODE", "CITY", "STATE", "ADDRESS", "PHONE", "DISTRICT"]
   
   def initialize
-    @api_key = File.read "./config/api_key.txt"
-    @client = Congress::Client.new(@api_key)
-    @data = []
+    @client = Congress::Client.new(File.read "./config/api_key.txt")
     @contents = nil
     @queue_results = []
     @final_table = nil
     @district = district
-    @html_template = File.read "table_template.erb"
   end
   
   def all_entries(filename="./event_attendees.csv")
@@ -32,7 +29,7 @@ class QueueHolder < Loader
   end
   
   def queue_count
-    queue_count = @queue_results.count 
+    @queue_results.count 
   end
     
   def queue_clear
@@ -42,7 +39,6 @@ class QueueHolder < Loader
   def queue_district(zipcode=queue_results[0].zipcode)
     if @queue_results.count < 10
       found_district = @client.districts_locate(zipcode)[:results][0][:district].to_s
-      found_district
       @district = found_district
     else
       "Sorry, too many entries."
@@ -82,27 +78,59 @@ class QueueHolder < Loader
     end
   end
   
-  def html_directory_generator(filename)
-      Dir.mkdir("output") && (File.rename "./lib/stylesheet.css", "./output/stylesheet.css") unless Dir.exists? "output"
-      output_dir = Dir["./output"]    
-      File.open("#{filename}.html", 'w') do |file|
-        file.puts @html_output
-      end
-    
+  def queue_export_html(filename="QueueOutput")
+    Dir.mkdir("output") && (File.rename "./lib/stylesheet.css", "./output/stylesheet.css") unless Dir.exists? "output"
+    export_name = "./output/filename.html"
+    queue_output_creator
+    File.open(export_name, 'w') do |file|
+      file.puts @queue_output
+    end
+    css_file_regenerator
   end
   
-  def queue_export_html(filename="QueueOutput")
-      erb_template = ERB.new @html_template
-      html_output = erb_template.result(binding)
-      current_date = "#{Time.now.strftime("%d/%m/%Y at %H:%M")}"
-      html_directory_generator(filename)
-      css_file = File.read "./output/stylesheet.css"
-      File.open("./lib/stylesheet.css", 'w') do |file|
-        file.puts css_file
-      end
-      puts "HTML file exported with the name #{filename}.html."
-      exit
+  def queue_output_creator
+    file_template = File.read "./lib/table_template.html.erb"
+    erb_template = ERB.new file_template
+    exporter = erb_template.result(binding)
+    @queue_output = exporter
   end
+  
+  def css_file_regenerator
+    css_file = File.read "output/stylesheet.css"
+    File.open("./lib/stylesheet.css", 'w') do |file|
+      file.puts css_file
+    end
+  end
+  
+  
+  
+  
+  # def html_directory_generator(filename)
+  #     Dir.mkdir("output") && (File.rename "./lib/stylesheet.css", "./output/stylesheet.css") unless Dir.exists? "output"
+  #     output_dir = Dir["./output"]    
+  #     File.open("#{filename}.html", 'w') do |file|
+  #       file.puts html_output
+  #     end
+  #   
+  # end
+  # 
+  
+  # 
+  # def queue_export_html(filename="output/QueueOutput")
+  #   @queue_results.each do |row|
+  #     erb_template = ERB.new @html_template
+  #     html_output = erb_template.result(binding)
+  #     
+  #     current_date = "#{Time.now.strftime("%d/%m/%Y at %H:%M")}"
+  #     html_directory_generator(filename)
+  #     css_file_regenerator
+  #     
+  #   end
+  #     puts "HTML file exported with the name #{filename}.html."
+  #     exit
+  # end
+  
+  
 end
 
   
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -6,7 +6,7 @@ class QueueHolderTest < Minitest::Test
   def test_queue_has_an_api_key
     q = QueueHolder.new
     tester = File.read "./config/api_key.txt"
-    assert_equal tester, q.api_key
+    assert_equal tester, q.client.key
   end
   
   def test_queue_can_access_api
@@ -41,7 +41,7 @@ class QueueHolderTest < Minitest::Test
     q.find("zipcode", "20010")
     assert_equal 1, q.queue_count
     q.queue_district
-    assert_equal "District is 0", q.queue_district
+    assert_equal "0", q.queue_district
   end
   
   def test_if_less_than_ten_entries_queue_looks_for_district_and_doesnt_just_return_default
@@ -51,7 +51,7 @@ class QueueHolderTest < Minitest::Test
     q.find("zipcode", "94611")
     assert_equal 1, q.queue_results.count
     q.queue_district
-    assert_equal "District is 13", q.queue_district
+    assert_equal "13", q.queue_district
   end
   
   def test_if_more_than_ten_entries_queue_will_not_look_for_districts
